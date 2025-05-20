7dc4973ccab80b4e71605723635d5e522b1e5fa0
Ritik Singh <139681422+ritik1803@users.noreply.github.com>
2025-05-20 17:58:02 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -15,16 +15,4 @@ const taskSchema = new mongoose.Schema({
 module.exports = mongoose.model('task', taskSchema);
 
 
-const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-    title: String,
-    details: String,
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
-});
-
-module.exports = mongoose.model('task', taskSchema);
