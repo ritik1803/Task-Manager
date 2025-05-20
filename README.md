8c77a0f2e7bcc7921db2bafc7cb845d9e8a7579f
unknown <singhritik2251@gmail.com>
2025-05-20 16:59:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4d25d5b2-69c5-4c11-b87e-465a54e6a179/deploy-status)](https://app.netlify.com/sites/taskmanager111/deploys)
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://singhritik2251:RO8VUXKEs4nOJyT4@cluster0.ijongm1.mongodb.net/first?retryWrites=true&w=majority&appName=Cluster0')
+
+    .then(() => console.log("Connected to MongoDB"));
+
+const userSchema= mongoose.Schema({
+    username:String,
+    name:String,
+    email:String,
+    password: String,
+    tasks:[
+        {
+           type: mongoose.Schema.Types.ObjectId, ref:"task"       
+        }
+    ]
+});
+
+module.exports=mongoose.model('user',userSchema)
