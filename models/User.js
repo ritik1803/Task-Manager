863f8b210e2e51519afd9073222e62995843850a
Ritik Singh <139681422+ritik1803@users.noreply.github.com>
2025-05-20 17:57:43 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,37 +1,23 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb+srv://singhritik2251:RO8VUXKEs4nOJyT4@cluster0.ijongm1.mongodb.net/first?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
-    .then(() => console.log("Connected to MongoDB"));
-
-const userSchema= mongoose.Schema({
-    username:String,
-    name:String,
-    email:String,
+// Define the user schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    name: String,
+    email: String,
     password: String,
-    tasks:[
+    tasks: [
         {
-           type: mongoose.Schema.Types.ObjectId, ref:"task"       
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "task"
         }
     ]
 });
 
-const { type } = require('express/lib/response');
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/first')
-    .then(() => console.log("Connected to MongoDB"));
-
-const userSchema= mongoose.Schema({
-    username:String,
-    name:String,
-    email:String,
-    password: String,
-    tasks:[
-        {
-           type: mongoose.Schema.Types.ObjectId, ref:"task"       
-        }
-    ]
-});
-
-module.exports=mongoose.model('user',userSchema)
+// Export the user model
+module.exports = mongoose.model('user', userSchema);
